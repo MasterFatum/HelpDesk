595832d51bab405fc68a536f9f15be3700555dbc
Pi Volt <master_Fatum@hotmail.com>
2018-05-30 20:53:35 +0300

    ттт

---
+++ MvcLesson/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcLesson.Models;
+
+namespace MvcLesson.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро!" : "Добрый день!";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult HelpDeskForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult HelpDeskForm(GuestResponse guest)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guest);
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+    }
+}
---
+++ MvcLesson/Models/GuestResponse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcLesson.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Введите своё имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Введите e-mail")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Вы ввели некорректный Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Введите номер телефона")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Введите номер кабинета")]
+        public string Location { get; set; }
+
+        [Required(ErrorMessage = "Выберите приоритет")]
+        public bool? Priority { get; set; }
+
+        [Required(ErrorMessage = "Выберите IT-отдел")]
+        public string Department { get; set; }
+
+        [Required(ErrorMessage = "Введите описание проблемы")]
+        public string BodyEmail { get; set; }
+
+        
+
+    }
+}
